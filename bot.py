dd340fad0d83e9102a4871314a120f79488a4250
NobodyOW <44954933+NobodyOW@users.noreply.github.com>
2019-07-25 10:52:37 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
+from random import randint
 
 BOT_PREFIX = os.environ['prefix']
 TOKEN = os.environ['token']
@@ -71,22 +72,32 @@ async def OwO(ctx):
 
 @bot.command()
 async def copypasta(ctx):
-    await ctx.send("What the fuck did you just fucking say about me, you little bitch? I'll have you know "
-                   "I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on "
-                   "Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top "
-                   "sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe "
-                   "you the fuck out with precision the likes of which has never been seen before on this Earth, mark "
-                   "my fucking words. You think you can get away with saying that shit to me over the Internet? Think "
-                   "again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP "
-                   "is being traced right now so you better prepare for the storm, maggot. The storm that wipes out "
-                   "the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime,"
-                   " and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I "
-                   "extensively trained in unarmed combat, but I have access to the entire arsenal of the United "
-                   "States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face "
-                   "of the continent, you little shit. If only you could have known what unholy retribution your "
-                   "little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking "
-                   "tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will "
-                   "shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
+    a = random.randint(1,2)
+    if a == 1:
+        await ctx.send("What the fuck did you just fucking say about me, you little bitch? I'll have you know "
+                       "I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on "
+                       "Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top "
+                       "sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe "
+                       "you the fuck out with precision the likes of which has never been seen before on this Earth, mark "
+                       "my fucking words. You think you can get away with saying that shit to me over the Internet? Think "
+                       "again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP "
+                       "is being traced right now so you better prepare for the storm, maggot. The storm that wipes out "
+                       "the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime,"
+                       " and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I "
+                       "extensively trained in unarmed combat, but I have access to the entire arsenal of the United "
+                       "States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face "
+                       "of the continent, you little shit. If only you could have known what unholy retribution your "
+                       "little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking "
+                       "tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will "
+                       "shit fury all over you and you will drown in it. You're fucking dead, kiddo.")
+    if a == 2:
+        await ctx.send("The Watts–Strogatz model is a random graph generation model"
+                       "that produces graphs with small-world properties, including "
+                       "short average path lengths and high clustering. It was proposed "
+                       "by Duncan J. Watts and Steven Strogatz in their joint 1998 Nature "
+                       "paper.[1] The model also became known as the (Watts) beta model "
+                       "after Watts used {\displaystyle \beta } \beta  to formulate it "
+                       "in his popular science book Six Degrees."
 
 
 bot.run(TOKEN)
