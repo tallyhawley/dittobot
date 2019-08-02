8093059cd3f3cd56567225995906a56f9479e6e0
isaac hoffman <trsileneh@gmail.com>
2019-08-02 00:47:30 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ generators = {}
 
 swears = ["fuck", "shit", "bitch", "pussy", "dick", "cunt", "whore", "cock", "piss"]
 prefixes = ['!', '.', ';', '?', '$', '%', '^', '&', '*', '/', ',', '~', '-', '+', '>', '<']
-banned_words = ["gusting", "t.co", "humidity", "#", "@", "&amp;", "&lt;", "ud", "temperature", "barometer", "graffiti tracking", "nigga"]
+banned_words = ['gusting', 't.co', 'humidity', '#', '@', '&amp;', '&lt;', 'ud', 'temperature', 'barometer', 'graffiti tracking', 'nigga', '°f']
 
 url = 'https://dittobot.s3-us-west-1.amazonaws.com/dashboard_x_usa_x_filter_nativeretweets.csv'
 tweets = urlopen(url)
